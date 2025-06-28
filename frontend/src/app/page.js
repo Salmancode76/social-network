dca06f3dc57b189ab5d414bd60a5dc4e07c1204a
hamad abdulrahman <hamad.ar@icloud.com>
2025-06-28 13:10:54 +0300

    chat redone

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState, Suspense } from "react";
+import ChatBox from "../components/ChatBox.js";
 import Link from "next/link";
 import { FetchAllPosts } from "./utils/FetchAllPosts";
 import { Internal505 } from "./Errors/page";
@@ -82,6 +83,7 @@ export default function Home() {
             )}
           </>
         )}
+        <ChatBox />
       </div>
     </Suspense>
   );
