a9261c705766493ef9b30ed0eb0d40cc35b3ed33
hamad abdulrahman <hamad.ar@icloud.com>
2025-07-05 18:04:29 +0300

    chatting screen done

--- frontend/src/app/components/ChatUser.js
+++ frontend/src/app/components/ChatUser.js
@@ -1,9 +1,28 @@
+import React, { useState } from 'react';
+import ChattingScreen from './ChattingScreen'; // Import the new component
+
 export default function ChatUser({ user }) {
+  const [showChattingScreen, setShowChattingScreen] = useState(false);
+
+  const handleUserClick = () => {
+    setShowChattingScreen(true);
+  };
+
+  const handleCloseChattingScreen = () => {
+    setShowChattingScreen(false);
+  };
+
   return (
-    <div className="chat-user">
-      <div className="chat-user-info">
-        <strong>{user}</strong>
+    <>
+      <div className="chat-user" onClick={handleUserClick}>
+        <div className="chat-user-info">
+          <strong>{user}</strong>
+        </div>
       </div>
-    </div>
+
+      {showChattingScreen && (
+        <ChattingScreen userName={user} onClose={handleCloseChattingScreen} />
+      )}
+    </>
   );
 }
